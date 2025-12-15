6c3123c50ac893357d67ab4474ca34d08a07ed30
subhadip07 <subhadipbag18@outlook.com>
2025-12-15 21:32:46 +0530

    update: pipeline/data_ingestion_pipeline.py -> data ingestion

---
+++ src/text_summarizer/pipeline/data_ingestion_pipeline.py
@@ -0,0 +1,16 @@
+from src.text_summarizer.config.configuration import ConfigurationManager
+from src.text_summarizer.components.data_ingestion import DataIngestion
+from text_summarizer.logging import logger
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_data_ingestion(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+        
