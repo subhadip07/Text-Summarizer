2430aa8eea2feb8723d85d0688792bbbc8fe2f5b
subhadip07 <subhadipbag18@outlook.com>
2025-12-19 21:29:32 +0530

    add: pipeline/data_transformation_pipeline.py -> data transformation

---
+++ src/text_summarizer/pipeline/data_transformation_pipeline.py
@@ -0,0 +1,14 @@
+from src.text_summarizer.config.configuration import ConfigurationManager
+from src.text_summarizer.components.data_transformation import DataTransformation
+from src.text_summarizer.logging import logger
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_data_transformation(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.convert()
+        
