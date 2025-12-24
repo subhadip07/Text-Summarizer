d6a75b91400808c46b7c29e911f0c5ea0c8c1a2c
subhadip07 <subhadipbag18@outlook.com>
2025-12-24 21:39:04 +0530

    update: entity/__init__.py -> Model Trainer

--- src/text_summarizer/entity/__init__.py
+++ src/text_summarizer/entity/__init__.py
@@ -14,3 +14,19 @@ class DataTransformationConfig:
     data_path: Path
     tokenizer_name: Path
     
+
+@dataclass
+class ModelTrainerConfig:
+    root_dir: Path
+    data_path: Path
+    model_ckpt: Path
+    num_train_epochs: int
+    warmup_steps: int
+    per_device_train_batch_size: int
+    weight_decay: float
+    logging_steps: int
+    evaluation_strategy: str
+    save_steps: float
+    eval_steps: int
+    gradient_accumulation_steps: int
+    
